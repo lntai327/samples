715265f39a08805295bcb6c2be0a7ea376a6de33
ericwindmill <ericwindmill@users.noreply.github.com>
2024-12-05 13:46:59 +0000

    deploy: 90cd385ef47548dcb7720781e805d5156a710604

--- web/form_app/flutter_bootstrap.js
+++ web/form_app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1280563039"
+    serviceWorkerVersion: "2132817037"
   }
 });
