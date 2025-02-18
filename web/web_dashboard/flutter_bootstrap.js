6e317586a284e2d9ffedcb32c3d8d99bcf0e557e
kevmoo <kevmoo@users.noreply.github.com>
2025-02-18 15:55:16 +0000

    deploy: 6cf81895d119e2402480843392eb5c46805718a5

--- web/web_dashboard/flutter_bootstrap.js
+++ web/web_dashboard/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "618763655"
+    serviceWorkerVersion: "2025110511"
   }
 });
